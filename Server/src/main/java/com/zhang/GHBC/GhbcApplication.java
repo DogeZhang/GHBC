b8be1d96bab65477a82a21f87c4832711800d524
DogeZhang <zhangyz86@hotmail.com>
2018-06-28 20:05:11 +0800

    Initial commit

---
+++ Server/src/main/java/com/zhang/GHBC/GhbcApplication.java
@@ -0,0 +1,12 @@
+package com.zhang.GHBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhbcApplication.class, args);
+	}
+}
---
+++ Server/src/test/java/com/zhang/GHBC/GhbcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhang.GHBC;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GhbcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
